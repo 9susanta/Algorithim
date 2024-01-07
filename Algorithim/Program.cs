bea690848bf66a144641836e8cf9c369f89c1729
susanta rout <rsusanta0@gmail.com>
2024-01-07 21:35:53 +0530

    #Day - 7 array product multiplication except the item

--- Algorithim/Program.cs
+++ Algorithim/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ProductOfArrayExceptItself;
 using ReverseVowel;
 using ReverseWordsString;
 
 Console.WriteLine("Hello, World!");
-ReverseWord.ReverseWords("a good   example");
+Product.ProductExceptSelf([1, 1, 3, 4]);
---
+++ ProductOfArrayExceptItself/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductOfArrayExceptItself
+{
+    public class Product
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int i = 0,numProduct=1;
+            int[] productArray=new int[nums.Length];
+            for(i = 0; i < nums.Length; i++)
+            {
+                //1,1,1,3
+                productArray[i] = numProduct;
+                Console.WriteLine(productArray[i]);
+                numProduct*=nums[i];
+            }
+            Console.WriteLine("---------------------");
+            numProduct = 1;
+            for (i = nums.Length-1; i >=0; i--)
+            {
+                //3
+                productArray[i] *= numProduct;
+                Console.WriteLine(productArray[i]);
+                numProduct *= nums[i];
+            }
+            return productArray;
+        }
+    }
+}
